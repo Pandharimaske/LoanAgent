ff94acb98bfd04b377943f86cd09323e0abe9aee
Onkar-0509 <onkarhadadare05@gmail.com>
2026-03-27 15:17:58 +0530

    frontend setup

---
+++ src/component/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      Onkar Hadadare
+    </div>
+  )
+}
+
+export default login
---
+++ src/component/signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signup = () => {
+  return (
+    <div className='text-3xl font-bold underline'>
+      Onkar
+    </div>
+  )
+}
+
+export default signup
